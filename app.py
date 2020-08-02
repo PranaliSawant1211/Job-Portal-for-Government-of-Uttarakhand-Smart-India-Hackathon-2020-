1548c8d4c33667721ebcc46e38f13f4647e66e61
Neha308 <49152389+Neha308@users.noreply.github.com>
2020-08-02 11:27:24 +0530

    profile pic bugs
    profile pic bugs

--- app.py
+++ app.py
@@ -990,6 +990,8 @@ def cdashboarddetail():
 def companydetails():
 	uname=session['comp_username']
 
+	
+
 
 	cursorskl = mysql.connection.cursor()
 	result2 = cursorskl.execute("SELECT * FROM award WHERE compid = %s", [uname])
